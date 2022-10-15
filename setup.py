bad0e48deb51ded449d6462dec0c22205db59d38
Cezary <249093@edu.p.lodz.pl>
2022-10-15 10:30:14 +0200

    basic module setup

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-# 
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    req = f.read().splitlines()
+
+setup(
+    name='master-thesis',
+    version='0-1',
+    description='',
+    author_='Cezary Kręt',
+    author_email='CezaryKret0@gmail.com',
+    url='https://github.com/CezaryKretGit/Master-Thesis',
+    packages=find_packages(exclude=('tests*', 'docs*', 'scripts*')),
+    required=req
+)
---
+++ thesis/core.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+
+class Pipeline:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print(np.__version__)
+    print(pd.__version__)
+    exit()
